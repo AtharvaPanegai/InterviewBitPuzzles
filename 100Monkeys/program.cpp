feb7d5df2076dc146b6a66d8629a3a8041062762
AtharvaPanegai <panegai.atharv@gmail.com>
2021-11-11 18:55:59 +0530

    First commit

---
+++ 100Monkeys/program.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+
+    // your code goes here
+
+    int arr[101] = {0};
+
+    for (int i = 1; i <= 100; i++)
+    {
+
+        for (int j = i; j <= 100; j += i)
+        {
+
+            if (arr[j] == 0)
+            {
+
+                arr[j] = 1;
+            }
+
+            else
+            {
+
+                arr[j] = 0;
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 1; i < 101; i++)
+    {
+
+        if (arr[i] == 1)
+            ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
